be253814cb1aa8f855cda7c2836496c200aa53fe
judithmg <jmartigo33@gmail.com>
2021-04-20 10:06:42 +0200

    Code for icons

---
+++ client/src/constants/icons.ts
@@ -0,0 +1,10 @@
+const icons = {
+    calendar: '&#xF073;',
+    bill: '&#xF53a;',
+    clock: '&#xF2;&#xF2;',
+    skull: '&#xF54c;',
+    syringe: '&#xF48e;',
+    palette: '&#xF53f;',
+};
+
+export default icons;
