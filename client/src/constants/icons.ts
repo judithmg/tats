7b9481fdda34610c5b2114d21a6306f037ff941b
judithmg <jmartigo33@gmail.com>
2021-04-22 16:20:46 +0200

    Code for font awesome icons

--- client/src/constants/icons.ts
+++ client/src/constants/icons.ts
@@ -5,11 +5,15 @@ const icons = {
     skull: '&#xF54c;',
     syringe: '&#xF48e;',
     palette: '&#xF53f;',
-    heart: '&#xF004;',
+    heart: '&#xf004;',
     map: '&#xF3c5;',
     share: '&#xF1e0;',
     comment: '&#xF27a;',
     hash: '&#xF3ef;',
+    home: '&#xf015;',
+    search: '&#xf002;',
+    plus: '&#xf0fe;',
+    user: '&#xf007;',
 };
 
 export default icons;
