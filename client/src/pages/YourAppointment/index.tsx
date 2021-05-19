53e0dd863d348c2e6e91cb3fe33b9fe5ea71b9b4
judithmg <jmartigo33@gmail.com>
2021-05-19 17:08:10 +0200

    add button notes

--- client/src/pages/YourAppointment/index.tsx
+++ client/src/pages/YourAppointment/index.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react';
 import InputRound from '../../components/Inputs/InputRound';
 import Header from '../../components/Headers/Popup';
 import '../../styles/YourAppointment.scss';
+import SimpleButton from '../../components/Buttons/SimpleButton';
 
 interface Props {
     classnames?: string;
@@ -12,6 +13,9 @@ const YourAppointment: FC<Props> = ({ classnames = '' }) => {
         <div className={`appointment ${classnames}`}>
             <Header title="Your appointment" />
             <InputRound color="lilac" placeholder="Add keywords..." />
+            {/* hacer un componente con cada pregunta, las estrellas se obtendrían con un GET, por props se pasa el título de la pregunta
+            el tipo de pregunta (stars/checkbox)
+            */}
             <p>
                 Was the appointment on time?
                 <span>&#xF005;</span>
@@ -36,6 +40,13 @@ const YourAppointment: FC<Props> = ({ classnames = '' }) => {
                 <span>&#xF005;</span>
                 <span>&#xF005;</span>
             </p>
+            <p>
+                <span>&#xf14a;</span>
+                Did the artist have music on?
+            </p>
+            <div className="appointment-btn">
+                <SimpleButton text="Add" color="lilac" />
+            </div>
         </div>
     );
 };
