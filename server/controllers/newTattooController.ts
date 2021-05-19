56d65e7019198273c5934ae8773fadf53eba2baa
judithmg <jmartigo33@gmail.com>
2021-05-19 18:44:37 +0200

    controllers

--- server/controllers/newTattooController.ts
+++ server/controllers/newTattooController.ts
@@ -1,3 +1,7 @@
 import { Request, Response } from "express";
 
 const { Tattoos } = require("../models");
+
+const postNewTattoo = async (req: Request, res: Response) => {
+  const { tattoo, user } = req.body;
+};
